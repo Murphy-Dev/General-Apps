9fdc2e5d692633f1b0cb4cbd4a14e427d8eea41e
Alto Lawden <95763262+Murphy-Dev@users.noreply.github.com>
2023-11-10 17:39:05 -0500

    Update src.py

--- Alt Bot/src.py
+++ Alt Bot/src.py
@@ -1,74 +1,99 @@
 import random
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+from typing import Any
 import pystyle
 import time
 import psutil
-
-alts = [
-    # Put the .rbxcookie's of your alts here.
+alts: list[str] = [
+  # Put the .rbxcookie's of your alts here.
 ]
 
 def find():
-    return alts[random.randint(0, len(alts) - 1)]
+  return alts[random.randint(0, len(alts) - 1)]
 
-attempt = 0
+attempt: int = 0
 
-def join(session, game, job):
-    if attempt > 5:
-        print("Rate limit has been reached! Please fix your Roblox player before trying again.")
-        return
+async def join(game: int, job: int) -> None:
+  if (attempt > 5):
+    print("Rate limit has been reached! Please fix your roblox player before trying again.")
+  session = webdriver.Chrome()
+  session.get("https://roblox.com")
+  session.add_cookie({'name': '.ROBLOSECURITY', 'value': find()})
+  session.get(f"roblox://game/PlaceLauncher.aspx?placeId={str(game)}&gameId={str(job) or 0}")
+  time.sleep(1)
+  if ("RobloxPlayerBeta.exe" not in list(psutil.process_iter())):
+    print("Alt Bot could not open RobloxPlayer! Please retry")
     global attempt
-    attempt = 0
+    attempt += 1
+    await join(game, job)
+  session.close()
 
-    session.add_cookie({'name': '.ROBLOSECURITY', 'value': find()})
-    session.get(f"roblox://game/PlaceLauncher.aspx?placeId={str(game)}&gameId={str(job) or 0}")
-    try:
-        WebDriverWait(session, 10).until(EC.presence_of_element_located((By.ID, "game-container")))
-        print("Joined game!")
-    except:
-        print("Alt Bot could not join the game! Please retry")
+def makefile(fil: str, data: Any):
+  with open(fil, 'w') as file:
+    file.write(data)
+    file.close()
 
-    while True:
-        if "RobloxPlayerBeta.exe" not in [program.name() for program in psutil.process_iter()]:
-            res = int(input(pystyle.Center.XCenter("""
-            You have closed the game!
-            Do you want to rejoin?
-            [1] Yes
-            [2] No
-            """)))
-            if res == 1:
-                await join()
-            else:
-                return
+def writefile(fil: str) -> str:
+  with open(fil, 'r') as file:
+    res = fil
+    file.close()
+    return res
 
-def main():
+
+
+print(pystyle.Center.XCenter("""
+  =============================
+  Roblox ALT-BOT V1
+  Made by Alto
+  =============================
+"""))
+
+async def main():
+
+  option = int(input(pystyle.Center.XCenter("""
+  Please input the game you want to join >>>
+  """)))
+  server = int(input(pystyle.Center.XCenter("""
+  Do you want to join a specific server?
+  [1] Yes
+  [2] No
+  """)))
+
+  if (server == 2):
+    await join(option, 0)
     print(pystyle.Center.XCenter("""
-    =============================
-    ALT-BOT V1
-    Made by Alto
-    =============================
+    Joined game!
     """))
-
-    option = int(input(pystyle.Center.XCenter("""
-    Please input the game you want to join >>>
-    """)))
-    server = int(input(pystyle.Center.XCenter("""
-    Do you want to join a specific server?
-    [1] Yes
-    [2] No
-    """)))
-
-    session = webdriver.Chrome()
-
-    if server == 2:
-        join(session, option, 0)
-    else:
-        srv = input(pystyle.Center.XCenter("What server?"))
-        join(session, option, srv)
-
-    session.quit()
-
+    while True:
+      does = False
+      for program in psutil.process_iter():
+        does = (program.name == "RobloxPlayerBeta.exe")
+      if (not does):
+        res = int(print(pystyle.Center.XCenter("""
+        You have closed the game!
+        Do you want to rejoin?
+        [1] Yes
+        [2] No
+        """)))
+        if (res == 1):
+          await main()
+  else:
+      srv: int =  int(input(pystyle.Center.XCenter("What server?")))
+      await join(option, srv)
+      print(pystyle.Center.XCenter("""
+      Joined game!
+      """))
+      while True:
+        does = False
+        for program in psutil.process_iter():
+          does = (program.name == "RobloxPlayerBeta.exe")
+        if (not does):
+          res = int(print(pystyle.Center.XCenter("""
+          You have closed the game!
+          Do you want to rejoin?
+          [1] Yes
+          [2] No
+          """)))
+          if (res == 1):
+            await main()
 main()
